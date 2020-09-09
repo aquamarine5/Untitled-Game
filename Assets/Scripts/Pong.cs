18339da7ef95ced6401e1e2fd52b7f5e78702b1a
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2020-09-09 20:22:02 +0800

    a 0.1.4-ot 测试 弹力

---
+++ Assets/Scripts/Pong.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pong : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d.AddForce(new Vector2(25,50));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Tilemap t;
+    public AnimatedTile tt;
+    void Start()
+    {
+        var a = new Grid();
+        Debug.Log(t.GetTile(new Vector3Int(1,1,0)));
+        t.SetEditorPreviewTile(new Vector3Int(1, 1, 0), tt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
