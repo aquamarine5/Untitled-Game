7e63d09ff1e34593556ff6c05d1001d62ac5d5f7
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2020-09-13 16:11:51 +0800

    更新

---
+++ Assets/Scripts/WeaponAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttack : MonoBehaviour
+{
+    public AttackType at;
+    public Test[] ts;
+    public int a;
+    public enum AttackType
+    {
+        Melee=1
+    }
+    public struct Test
+    {
+        public int a;
+        string b;
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
