4d5fe23b368ade51c4bfca64001bfcf6928b817b
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2020-09-14 22:30:06 +0800

    event learning

---
+++ Assets/Scripts/WeaponBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class WeaponBase
+{
+    public enum AttackType
+    {
+        Melee = 1
+    }
+    
+    
+}
+class Test
+{
+    public delegate void DelegateWeaponAttack();
+    public event DelegateWeaponAttack EventWeaponAttck;
+}
+class main
+{
+    void m()
+    {
+        Test test = new Test();
+        test.EventWeaponAttck += a;
+    }
+    void a() { }
+    
+}
