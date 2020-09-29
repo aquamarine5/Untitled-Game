3b7ef48998b18d759a85358b6e64f69ab462c670
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2020-09-29 20:06:09 +0800

    Add Mirror UnityPackage

---
+++ Assets/Mirror/Editor/Weaver/Processors/ReaderWriterProcessor.cs
@@ -0,0 +1,101 @@
+// finds all readers and writers and register them
+using System.IO;
+using Mono.CecilX;
+using UnityEditor.Compilation;
+
+namespace Mirror.Weaver
+{
+    public static class ReaderWriterProcessor
+    {
+        public static void Process(AssemblyDefinition CurrentAssembly)
+        {
+            Readers.Init();
+            Writers.Init();
+
+            foreach (Assembly unityAsm in CompilationPipeline.GetAssemblies())
+            {
+                if (unityAsm.name != CurrentAssembly.Name.Name)
+                {
+                    try
+                    {
+                        using (DefaultAssemblyResolver asmResolver = new DefaultAssemblyResolver())
+                        using (AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(unityAsm.outputPath, new ReaderParameters { ReadWrite = false, ReadSymbols = false, AssemblyResolver = asmResolver }))
+                        {
+                            ProcessAssemblyClasses(CurrentAssembly, assembly);
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        // During first import,  this gets called before some assemblies
+                        // are built,  just skip them
+                    }
+                }
+            }
+
+            ProcessAssemblyClasses(CurrentAssembly, CurrentAssembly);
+        }
+
+        static void ProcessAssemblyClasses(AssemblyDefinition CurrentAssembly, AssemblyDefinition assembly)
+        {
+            foreach (TypeDefinition klass in assembly.MainModule.Types)
+            {
+                // extension methods only live in static classes
+                // static classes are represented as sealed and abstract
+                if (klass.IsAbstract && klass.IsSealed)
+                {
+                    LoadDeclaredWriters(CurrentAssembly, klass);
+                    LoadDeclaredReaders(CurrentAssembly, klass);
+                }
+            }
+        }
+
+        static void LoadDeclaredWriters(AssemblyDefinition currentAssembly, TypeDefinition klass)
+        {
+            // register all the writers in this class.  Skip the ones with wrong signature
+            foreach (MethodDefinition method in klass.Methods)
+            {
+                if (method.Parameters.Count != 2)
+                    continue;
+
+                if (!method.Parameters[0].ParameterType.Is<NetworkWriter>())
+                    continue;
+
+                if (!method.ReturnType.Is(typeof(void)))
+                    continue;
+
+                if (!method.HasCustomAttribute<System.Runtime.CompilerServices.ExtensionAttribute>())
+                    continue;
+
+                if (method.HasGenericParameters)
+                    continue;
+
+                TypeReference dataType = method.Parameters[1].ParameterType;
+                Writers.Register(dataType, currentAssembly.MainModule.ImportReference(method));
+            }
+        }
+
+        static void LoadDeclaredReaders(AssemblyDefinition currentAssembly, TypeDefinition klass)
+        {
+            // register all the reader in this class.  Skip the ones with wrong signature
+            foreach (MethodDefinition method in klass.Methods)
+            {
+                if (method.Parameters.Count != 1)
+                    continue;
+
+                if (!method.Parameters[0].ParameterType.Is<NetworkReader>())
+                    continue;
+
+                if (method.ReturnType.Is(typeof(void)))
+                    continue;
+
+                if (!method.HasCustomAttribute<System.Runtime.CompilerServices.ExtensionAttribute>())
+                    continue;
+
+                if (method.HasGenericParameters)
+                    continue;
+
+                Readers.Register(method.ReturnType, currentAssembly.MainModule.ImportReference(method));
+            }
+        }
+    }
+}
---
+++ Assets/Mirror/Editor/Weaver/Weaver.cs
@@ -0,0 +1,340 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Mono.CecilX;
+using Mono.CecilX.Cil;
+
+namespace Mirror.Weaver
+{
+    // This data is flushed each time - if we are run multiple times in the same process/domain
+    class WeaverLists
+    {
+        // setter functions that replace [SyncVar] member variable references. dict<field, replacement>
+        public Dictionary<FieldDefinition, MethodDefinition> replacementSetterProperties = new Dictionary<FieldDefinition, MethodDefinition>();
+        // getter functions that replace [SyncVar] member variable references. dict<field, replacement>
+        public Dictionary<FieldDefinition, MethodDefinition> replacementGetterProperties = new Dictionary<FieldDefinition, MethodDefinition>();
+
+        public List<MethodDefinition> generatedReadFunctions = new List<MethodDefinition>();
+        public List<MethodDefinition> generatedWriteFunctions = new List<MethodDefinition>();
+
+        public TypeDefinition generateContainerClass;
+
+        // amount of SyncVars per class. dict<className, amount>
+        public Dictionary<string, int> numSyncVars = new Dictionary<string, int>();
+
+        public HashSet<string> ProcessedMessages = new HashSet<string>();
+
+
+        public int GetSyncVarStart(string className)
+        {
+            return numSyncVars.ContainsKey(className)
+                   ? numSyncVars[className]
+                   : 0;
+        }
+
+        public void SetNumSyncVars(string className, int num)
+        {
+            numSyncVars[className] = num;
+        }
+
+        public void ConfirmGeneratedCodeClass()
+        {
+            if (generateContainerClass == null)
+            {
+                generateContainerClass = new TypeDefinition("Mirror", "GeneratedNetworkCode",
+                        TypeAttributes.BeforeFieldInit | TypeAttributes.Class | TypeAttributes.AnsiClass | TypeAttributes.Public | TypeAttributes.AutoClass,
+                        WeaverTypes.Import<object>());
+
+                const MethodAttributes methodAttributes = MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName;
+                MethodDefinition method = new MethodDefinition(".ctor", methodAttributes, WeaverTypes.Import(typeof(void)));
+                method.Body.Instructions.Add(Instruction.Create(OpCodes.Ldarg_0));
+                method.Body.Instructions.Add(Instruction.Create(OpCodes.Call, Resolvers.ResolveMethod(WeaverTypes.Import<object>(), Weaver.CurrentAssembly, ".ctor")));
+                method.Body.Instructions.Add(Instruction.Create(OpCodes.Ret));
+
+                generateContainerClass.Methods.Add(method);
+            }
+        }
+    }
+
+    internal static class Weaver
+    {
+        public static string InvokeRpcPrefix => "InvokeUserCode_";
+
+        public static WeaverLists WeaveLists { get; private set; }
+        public static AssemblyDefinition CurrentAssembly { get; private set; }
+        public static bool WeavingFailed { get; private set; }
+        public static bool GenerateLogErrors { get; set; }
+
+        // private properties
+        static readonly bool DebugLogEnabled = true;
+
+        public static void DLog(TypeDefinition td, string fmt, params object[] args)
+        {
+            if (!DebugLogEnabled)
+                return;
+
+            Console.WriteLine("[" + td.Name + "] " + string.Format(fmt, args));
+        }
+
+        // display weaver error
+        // and mark process as failed
+        public static void Error(string message)
+        {
+            Log.Error(message);
+            WeavingFailed = true;
+        }
+
+        public static void Error(string message, MemberReference mr)
+        {
+            Log.Error($"{message} (at {mr})");
+            WeavingFailed = true;
+        }
+
+        public static void Warning(string message, MemberReference mr)
+        {
+            Log.Warning($"{message} (at {mr})");
+        }
+
+
+        static void CheckMonoBehaviour(TypeDefinition td)
+        {
+            if (td.IsDerivedFrom<UnityEngine.MonoBehaviour>())
+            {
+                MonoBehaviourProcessor.Process(td);
+            }
+        }
+
+        static bool WeaveNetworkBehavior(TypeDefinition td)
+        {
+            if (!td.IsClass)
+                return false;
+
+            if (!td.IsDerivedFrom<NetworkBehaviour>())
+            {
+                CheckMonoBehaviour(td);
+                return false;
+            }
+
+            // process this and base classes from parent to child order
+
+            List<TypeDefinition> behaviourClasses = new List<TypeDefinition>();
+
+            TypeDefinition parent = td;
+            while (parent != null)
+            {
+                if (parent.Is<NetworkBehaviour>())
+                {
+                    break;
+                }
+
+                try
+                {
+                    behaviourClasses.Insert(0, parent);
+                    parent = parent.BaseType.Resolve();
+                }
+                catch (AssemblyResolutionException)
+                {
+                    // this can happen for plugins.
+                    //Console.WriteLine("AssemblyResolutionException: "+ ex.ToString());
+                    break;
+                }
+            }
+
+            bool modified = false;
+            foreach (TypeDefinition behaviour in behaviourClasses)
+            {
+                modified |= new NetworkBehaviourProcessor(behaviour).Process();
+            }
+            return modified;
+        }
+
+        static bool WeaveMessage(TypeDefinition td)
+        {
+            if (!td.IsClass)
+                return false;
+
+            // already processed
+            if (WeaveLists.ProcessedMessages.Contains(td.FullName))
+                return false;
+
+            bool modified = false;
+
+            if (td.ImplementsInterface<IMessageBase>())
+            {
+                // process this and base classes from parent to child order
+                try
+                {
+                    TypeDefinition parent = td.BaseType.Resolve();
+                    // process parent
+                    WeaveMessage(parent);
+                }
+                catch (AssemblyResolutionException)
+                {
+                    // this can happen for plugins.
+                    //Console.WriteLine("AssemblyResolutionException: "+ ex.ToString());
+                }
+
+                // process this
+                MessageClassProcessor.Process(td);
+                WeaveLists.ProcessedMessages.Add(td.FullName);
+                modified = true;
+            }
+
+            // check for embedded types
+            // inner classes should be processed after outter class to avoid StackOverflowException
+            foreach (TypeDefinition embedded in td.NestedTypes)
+            {
+                modified |= WeaveMessage(embedded);
+            }
+
+            return modified;
+        }
+
+        static bool WeaveSyncObject(TypeDefinition td)
+        {
+            bool modified = false;
+
+            // ignore generic classes
+            // we can not process generic classes
+            // we give error if a generic syncObject is used in NetworkBehaviour
+            if (td.HasGenericParameters)
+                return false;
+
+            // ignore abstract classes
+            // we dont need to process abstract classes because classes that
+            // inherit from them will be processed instead
+
+            // We cant early return with non classes or Abstract classes
+            // because we still need to check for embeded types
+            if (td.IsClass || !td.IsAbstract)
+            {
+                if (td.IsDerivedFrom(typeof(SyncList<>)))
+                {
+                    SyncListProcessor.Process(td, WeaverTypes.Import(typeof(SyncList<>)));
+                    modified = true;
+                }
+                else if (td.IsDerivedFrom(typeof(SyncSet<>)))
+                {
+                    SyncListProcessor.Process(td, WeaverTypes.Import(typeof(SyncSet<>)));
+                    modified = true;
+                }
+                else if (td.IsDerivedFrom(typeof(SyncDictionary<,>)))
+                {
+                    SyncDictionaryProcessor.Process(td);
+                    modified = true;
+                }
+            }
+
+            // check for embedded types
+            foreach (TypeDefinition embedded in td.NestedTypes)
+            {
+                modified |= WeaveSyncObject(embedded);
+            }
+
+            return modified;
+        }
+
+        static bool WeaveModule(ModuleDefinition moduleDefinition)
+        {
+            try
+            {
+                bool modified = false;
+
+                // We need to do 2 passes, because SyncListStructs might be referenced from other modules, so we must make sure we generate them first.
+                System.Diagnostics.Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+                foreach (TypeDefinition td in moduleDefinition.Types)
+                {
+                    if (td.IsClass && td.BaseType.CanBeResolved())
+                    {
+                        modified |= WeaveSyncObject(td);
+                    }
+                }
+                watch.Stop();
+                Console.WriteLine("Weave sync objects took " + watch.ElapsedMilliseconds + " milliseconds");
+
+                watch.Start();
+                foreach (TypeDefinition td in moduleDefinition.Types)
+                {
+                    if (td.IsClass && td.BaseType.CanBeResolved())
+                    {
+                        modified |= WeaveNetworkBehavior(td);
+                        modified |= WeaveMessage(td);
+                        modified |= ServerClientAttributeProcessor.Process(td);
+                    }
+                }
+                watch.Stop();
+                Console.WriteLine("Weave behaviours and messages took" + watch.ElapsedMilliseconds + " milliseconds");
+
+
+                if (modified)
+                    PropertySiteProcessor.Process(moduleDefinition);
+
+                return modified;
+            }
+            catch (Exception ex)
+            {
+                Error(ex.ToString());
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        static bool Weave(string assName, IEnumerable<string> dependencies)
+        {
+            using (DefaultAssemblyResolver asmResolver = new DefaultAssemblyResolver())
+            using (CurrentAssembly = AssemblyDefinition.ReadAssembly(assName, new ReaderParameters { ReadWrite = true, ReadSymbols = true, AssemblyResolver = asmResolver }))
+            {
+                asmResolver.AddSearchDirectory(Path.GetDirectoryName(assName));
+                asmResolver.AddSearchDirectory(Helpers.UnityEngineDllDirectoryName());
+                if (dependencies != null)
+                {
+                    foreach (string path in dependencies)
+                    {
+                        asmResolver.AddSearchDirectory(path);
+                    }
+                }
+
+                WeaverTypes.SetupTargetTypes(CurrentAssembly);
+                System.Diagnostics.Stopwatch rwstopwatch = System.Diagnostics.Stopwatch.StartNew();
+                ReaderWriterProcessor.Process(CurrentAssembly);
+                rwstopwatch.Stop();
+                Console.WriteLine($"Find all reader and writers took {rwstopwatch.ElapsedMilliseconds} milliseconds");
+
+                ModuleDefinition moduleDefinition = CurrentAssembly.MainModule;
+                Console.WriteLine($"Script Module: {moduleDefinition.Name}");
+
+                bool modified = WeaveModule(moduleDefinition);
+
+                if (WeavingFailed)
+                {
+                    return false;
+                }
+
+                if (modified)
+                {
+                    // write to outputDir if specified, otherwise perform in-place write
+                    WriterParameters writeParams = new WriterParameters { WriteSymbols = true };
+                    CurrentAssembly.Write(writeParams);
+                }
+            }
+
+            return true;
+        }
+
+        public static bool WeaveAssembly(string assembly, IEnumerable<string> dependencies)
+        {
+            WeavingFailed = false;
+            WeaveLists = new WeaverLists();
+
+            try
+            {
+                return Weave(assembly, dependencies);
+            }
+            catch (Exception e)
+            {
+                Log.Error("Exception :" + e);
+                return false;
+            }
+        }
+
+    }
+}
