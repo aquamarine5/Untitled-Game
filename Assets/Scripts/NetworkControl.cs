8c6a7eac4dc40ac4e0c3041e0fb9aba6f0aab4c0
awesomehhhhh <50990424+awesomehhhhh@users.noreply.github.com>
2021-01-19 21:00:43 +0800

    Shadow Caseter 2D

--- Assets/Scripts/NetworkControl.cs
+++ Assets/Scripts/NetworkControl.cs
@@ -5,19 +5,25 @@ using Mirror;
 
 public class NetworkControl : NetworkManager
 {
+    public static NetworkControl S { get; private set; }
+
     [Header("Player Require Argument")]
     public Cinemachine.CinemachineVirtualCamera cinemachineVirtualCamera;
     public PlayerMove playerMove;
+
     [Space(10)]
     public GameObject localPlayer;
+    public override void Awake()
+    {
+        S = this;
+        base.Awake();
+    }
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         GameObject player = Instantiate(playerPrefab, GetStartPosition().position, new Quaternion());
         NetworkServer.AddPlayerForConnection(conn, player);
         if (conn.identity.isLocalPlayer) { 
             localPlayer = player;
-            cinemachineVirtualCamera.Follow = localPlayer.transform;
-            playerMove.rd2d = localPlayer.GetComponent<Rigidbody2D>();
         }
     }
 }
---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        NetworkControl.S.cinemachineVirtualCamera.Follow = transform;
+        NetworkControl.S.playerMove.rd2d = GetComponent<Rigidbody2D>();
+    }
+}
